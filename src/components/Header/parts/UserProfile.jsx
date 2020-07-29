72fd4f9482dfd789b2210944631d5f0cae93d691
kalysh_maxym <503292@ukr.net>
2020-07-29 20:03:57 +0300

    merge commit

--- src/components/Header/parts/UserProfile.jsx
+++ src/components/Header/parts/UserProfile.jsx
@@ -1,9 +1,31 @@
-import React from "react";
-
-import css from "./UserProfile.module.scss";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Dropdown } from 'react-bootstrap';
+import css from './UserProfile.module.scss';
 
 const UserProfile = ({ user }) => {
-  return <div className={`${css.wrapUserProfile}`}>{user.name}</div>;
-};
+  // eslint-disable-next-line react/prop-types
+  const { name } = user;
+  return (
+    <div className={`${css.wrapUserProfile}`}>
+      <Dropdown>
+        <Dropdown.Toggle variant="" id="dropdown-basic">
+          {name}
+        </Dropdown.Toggle>
 
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Profile</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Logout</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  );
+};
+UserProfile.propTypes = {
+  user: PropTypes.shape({}),
+  name: PropTypes.string.isRequired,
+};
+UserProfile.defaultProps = {
+  user: null,
+};
 export default UserProfile;
