8df77c9f98454637b1d2285673bc5ec51be4973c
kalysh_maxym <503292@ukr.net>
2020-07-30 19:20:57 +0300

    REFACT _ AlertMessage

--- src/components/Header/parts/AlertMessage.jsx
+++ src/components/Header/parts/AlertMessage.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from 'react';
 
-import css from "./AlertMessage.module.scss";
+import { Alert } from 'react-bootstrap';
+
+import css from './AlertMessage.module.scss';
 
 const AlertMessage = () => {
+  const [show, setShow] = useState(true);
   return (
-    <div className={css.wrapAlertMessage}>
-      <p>You have 14 free trial days left. Purchase now</p>
-    </div>
+    <>
+      {show && (
+        <div className={css.wrapAlertMessage}>
+          <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+            <p>You have 14 free trial days left. Purchase now</p>
+          </Alert>
+        </div>
+      )}
+    </>
   );
 };
 
