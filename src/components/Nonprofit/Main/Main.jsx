0de0520121d539911c8e271b1af440c3cc964740
503292 <503292@ukr.net>
2020-08-25 13:55:53 +0300

    ADD _ reviews carousel

--- src/components/Nonprofit/Main/Main.jsx
+++ src/components/Nonprofit/Main/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from './parts/Hero';
+import Reviews from './parts/Reviews';
 
 const Main = () => {
   return (
     <main>
       <Hero />
+      <Reviews />
     </main>
   );
 };
