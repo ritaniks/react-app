e3a05e683e1d9a4a3dff63b2d6519bf26571807e
503292 <503292@ukr.net>
2020-08-31 17:01:29 +0300

    some changr

---
+++ src/components/Nonprofit/Main/parts/NonProfOrg.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Carousel from '@brainhubeu/react-carousel';
+
+import ImageResize from './ImageResize';
+import { nonProfitOrg } from './imageHelper';
+
+const NonProfOrg = () => {
+    return (
+        
+    );
+}
+
+export default NonProfOrg;
