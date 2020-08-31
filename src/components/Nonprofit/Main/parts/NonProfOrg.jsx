5846a1d12ae719e97a5975eb6b3c044397d6f141
503292 <503292@ukr.net>
2020-08-31 17:01:59 +0300

    some change

--- src/components/Nonprofit/Main/parts/NonProfOrg.jsx
+++ src/components/Nonprofit/Main/parts/NonProfOrg.jsx
@@ -5,9 +5,7 @@ import ImageResize from './ImageResize';
 import { nonProfitOrg } from './imageHelper';
 
 const NonProfOrg = () => {
-    return (
-        
-    );
-}
+  return <>NonProfOrg</>;
+};
 
 export default NonProfOrg;
