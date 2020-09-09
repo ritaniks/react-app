438bb64af8cd77edffaec44b3dac9a8c8f459b2b
503292 <503292@ukr.net>
2020-09-09 14:18:05 +0300

    clean code

--- src/components/Nonprofit/Main/parts/ImageResize.jsx
+++ src/components/Nonprofit/Main/parts/ImageResize.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 const ImageResize = ({ path }) => {
   return (
     <picture>
-      {console.log(path, 'path')}
       <source media="(min-width: 1200px)" sizes="50vw" srcSet={path[1]} />
       <source media="(min-width: 992px)" srcSet={path[0]} />
       <img
