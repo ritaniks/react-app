dc9cf67ac7634cda1b73351f93835df0d3b02770
ritaniks <crisitna.tabak@gmail.com>
2020-09-21 20:58:57 +0300

    Photos and resize

--- src/components/Nonprofit/Main/parts/ImageResize.jsx
+++ src/components/Nonprofit/Main/parts/ImageResize.jsx
@@ -4,12 +4,11 @@ import PropTypes from 'prop-types';
 const ImageResize = ({ path }) => {
   return (
     <picture>
-      <source media="(min-width: 1200px)" sizes="50vw" srcSet={path[1]} />
-      <source media="(min-width: 992px)" srcSet={path[0]} />
+      <source media="(min-width: 768px)" srcSet={`${path[0]}, ${path[1]} 2x`} />
       <img
         src={path[2]}
         alt={path[0]}
-        srcSet={path[3]}
+        srcSet={(`${path[2]}`, `${path[3]} 2x`)}
         style={{ width: ' 100%' }}
       />
     </picture>
