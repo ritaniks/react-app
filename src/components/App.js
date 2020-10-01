845d3754426f60660522cd505849fd581a9c38de
503292 <503292@ukr.net>
2020-10-01 21:55:14 +0300

    ADD _ start config for Registration Form

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,23 @@ import React from 'react';
 
 import Header from './Header/Header';
 import Main from './Main/Main';
-import Nonprofit from './Nonprofit/Nonprofit';
+// import Nonprofit from './Nonprofit/Nonprofit';
+import Registration from './Registration/Registration';
 
 import css from './App.module.scss';
 
 const user = {
   name: 'Kristina',
   admin: true,
-  auth: true,
+  auth: false,
   users: [],
 };
 
 const App = () => {
   return (
     <div className={`${css.wrapApp}`}>
-      {!user.auth && <Nonprofit />}
+      {/* {!user.auth && <Nonprofit />} */}
+      {!user.auth && <Registration />}
 
       {user.auth && (
         <>
