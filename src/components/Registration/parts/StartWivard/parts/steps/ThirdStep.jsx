5142c8794af7c6de2d1f21ad376908b23505f8d8
503292 <503292@ukr.net>
2020-10-08 12:15:51 +0300

    ADD _ icon in Registraton components

--- src/components/Registration/parts/StartWivard/parts/steps/ThirdStep.jsx
+++ src/components/Registration/parts/StartWivard/parts/steps/ThirdStep.jsx
@@ -3,6 +3,12 @@ import React from 'react';
 import Submit from '../../../buttons/Submit';
 import PrevBtn from '../../../buttons/PrevBtn';
 
+import { ReactComponent as Pen } from '../../../../../../assets/img/registration/pen.svg';
+import { ReactComponent as X } from '../../../../../../assets/img/registration/x.svg';
+// import { ReactComponent as Folder } from '../../../../../../assets/img/registration/folder.svg';
+// import { ReactComponent as File } from '../../../../../../assets/img/registration/file-alt.svg';
+// import { ReactComponent as User } from '../../../../../../assets/img/registration/user.svg';
+
 import css from './steps.module.scss';
 
 const ThirdStep = ({ countClick, setCountClick }) => {
@@ -40,50 +46,11 @@ const ThirdStep = ({ countClick, setCountClick }) => {
           <ul id="items" className="list-group mb-3">
             <li className="list-group-item">
               Minh Hong
-              <button className="btn btn-danger btn-sm float-right delete">
-                X
-              </button>
               <button
-                className="btn btn-primary btn-sm float-right mr-3 project"
-                type="button"
-                data-toggle="modal"
-                data-target="#exampleModal"
+                className="btn btn-danger btn-sm float-right delete"
+                style={{ width: '2rem', height: '2rem' }}
               >
-                Add Project
-              </button>
-            </li>
-            <li className="list-group-item">
-              Mark Duffer
-              <button className="btn btn-danger btn-sm float-right delete">
-                X
-              </button>
-              <button
-                className="btn btn-primary btn-sm float-right mr-3 project"
-                type="button"
-                data-toggle="modal"
-                data-target="#exampleModal"
-              >
-                Add Project
-              </button>
-            </li>
-            <li className="list-group-item">
-              Client XYZ Corp
-              <button className="btn btn-danger btn-sm float-right delete">
-                X
-              </button>
-              <button
-                className="btn btn-primary btn-sm float-right mr-3 project"
-                type="button"
-                data-toggle="modal"
-                data-target="#exampleModal"
-              >
-                Add Project
-              </button>
-            </li>
-            <li className="list-group-item">
-              Mary Molloy
-              <button className="btn btn-danger btn-sm float-right delete">
-                X
+                <X />
               </button>
               <button
                 className="btn btn-primary btn-sm float-right mr-3 project"
@@ -98,82 +65,27 @@ const ThirdStep = ({ countClick, setCountClick }) => {
           <h5 className="title">Projects</h5>
           <ul className="list-group" id="items2">
             <li className="list-group-item">
-              <button className="btn btn-danger btn-sm float-right delete">
-                X
+              <button
+                className="btn btn-danger btn-sm float-right delete"
+                style={{ width: '2rem', height: '2rem' }}
+              >
+                <X />
               </button>
               <button
                 className="btn btn-primary btn-sm float-right mr-3 project"
                 type="button"
                 data-toggle="modal"
                 data-target="#exampleModal"
+                style={{ width: '2rem', height: '2rem' }}
               >
-                <i className="fas fa-pen edit-icon"></i>
+                <Pen />
+                {/* <i className="fas fa-pen edit-icon"></i> */}
               </button>
               <div className="pl-3">
                 <i className="far fa-user ml-n3"></i> Mark Duffer
               </div>
               <div className="ml-3a pl-3">
                 <i className="far fa-file-alt ml-n3"></i> General Legal Matters
-              </div>
-            </li>
-            <li className="list-group-item">
-              <button className="btn btn-danger btn-sm float-right delete">
-                X
-              </button>
-              <button
-                className="btn btn-primary btn-sm float-right mr-3 project"
-                type="button"
-                data-toggle="modal"
-                data-target="#exampleModal"
-              >
-                <i className="fas fa-pen edit-icon"></i>
-              </button>
-              <div className="pl-3">
-                <i className="far fa-user ml-n3"></i> Minh Hong (12 Investors)
-              </div>
-              <div className="ml-3a pl-3">
-                <i className="far fa-file-alt ml-n3"></i> Hong, et. al. v.
-                Suavei, Inc., Infante and Wong
-              </div>
-            </li>
-            <li className="list-group-item">
-              <button className="btn btn-danger btn-sm float-right delete">
-                X
-              </button>
-              <button
-                className="btn btn-primary btn-sm float-right mr-3 project"
-                type="button"
-                data-toggle="modal"
-                data-target="#exampleModal"
-              >
-                <i className="fas fa-pen edit-icon"></i>
-              </button>
-              <div className="ml-3a pl-custom">
-                <i className="far fa-folder ml-n-custom"></i> Web design for
-                West side project
-              </div>
-              <div className="ml-4a pl-3">
-                <i className="far fa-file-alt ml-n3"></i> Color matching
-              </div>
-            </li>
-            <li className="list-group-item">
-              <button className="btn btn-danger btn-sm float-right delete">
-                X
-              </button>
-              <button
-                className="btn btn-primary btn-sm float-right mr-3 project"
-                type="button"
-                data-toggle="modal"
-                data-target="#exampleModal"
-              >
-                <i className="fas fa-pen edit-icon"></i>
-              </button>
-              <div className="pl-3">
-                <i className="far fa-user ml-n3"></i> Mary Molloy
-              </div>
-              <div className="ml-3a pl-3">
-                <i className="far fa-file-alt ml-n3"></i> Molloy Family Trust
-                Dispute
               </div>
             </li>
           </ul>
