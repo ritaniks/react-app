5110c096cffe0c31ca5e179b6ee6885a3f89eea3
503292 <503292@ukr.net>
2020-10-19 19:04:44 +0300

    ADD _ propTypes for buttons / clean code

--- src/components/Registration/buttons/PrevBtn.jsx
+++ src/components/Registration/buttons/PrevBtn.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import css from './buttons.module.scss';
 
@@ -16,6 +17,11 @@ const PrevBtn = ({ countClick, setCountClick }) => {
       Previous
     </button>
   );
+};
+
+PrevBtn.propTypes = {
+  countClick: PropTypes.number.isRequired,
+  setCountClick: PropTypes.func.isRequired,
 };
 
 export default PrevBtn;
