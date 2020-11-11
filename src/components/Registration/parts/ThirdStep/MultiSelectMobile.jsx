a4d76e1723f58d65567f4114c88722f79c27563d
503292 <503292@ukr.net>
2020-11-11 17:22:12 +0200

    ADD _ in MultiSelect hook for width

--- src/components/Registration/parts/ThirdStep/MultiSelectMobile.jsx
+++ src/components/Registration/parts/ThirdStep/MultiSelectMobile.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import useWindowSize from '../../../hooks/useWindowSize';
 // import css from './MultiSelectMobile.module.scss';
 
+const tmp = {
+  managers: [
+    { name: 'Jack', checked: false, role: 'managers' },
+    { name: 'Kate', checked: false, role: 'managers' },
+    { name: 'Tom', checked: false, role: 'managers' },
+  ],
+
+  users: [
+    { name: 'Elis', checked: false, role: 'users' },
+    { name: 'Jonny', checked: false, role: 'users' },
+    { name: 'Christian', checked: false, role: 'users' },
+    { name: 'David', checked: false, role: 'users' },
+    { name: 'Carter', checked: false, role: 'users' },
+    { name: 'Evelyn', checked: false, role: 'users' },
+    { name: 'Scarlett', checked: false, role: 'users' },
+    { name: 'Eleanor', checked: false, role: 'users' },
+    { name: 'Kingston', checked: false, role: 'users' },
+    { name: 'Julian', checked: false, role: 'users' },
+    { name: 'Lincoln', checked: false, role: 'users' },
+    { name: 'Addison', checked: false, role: 'users' },
+  ],
+};
+
 const MultiSelectMobile = () => {
-  return <>MultiSelectMobile</>;
+  const widthDivice = useWindowSize().width;
+  return <div>MultiSelectMobile</div>;
 };
 
 export default MultiSelectMobile;
